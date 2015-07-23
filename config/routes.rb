173adbfa566a72bcd0c2d1c300fd89a84e2d457f
Jordan Zhang <jordanzhang@gmail.com>
2015-07-23 18:27:05 +0000

    Added admin feature

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   root to: 'movies#index'
 
+  namespace :admin do
+   resources :users
+  end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
