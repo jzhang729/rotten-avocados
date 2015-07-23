2b850e55623fc21fa48d15b0c7d687dd3065f124
Jordan Zhang <jordanzhang@gmail.com>
2015-07-23 18:33:42 +0000

    Added admin feature

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,30 @@
+class Admin::UsersController < ApplicationController
+
+  before_filter :restrict_access
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to movies_path, notice: "Welcome aboard, #{@user.firstname}!"
+    else
+      render :new
+    end
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation, :admin)
+  end
+
+end
